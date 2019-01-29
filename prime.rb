6886d573717388028a85100177f967b003f0f2ad
Louie O'Grady <l.ogrady@gmail.com>
2019-01-29 08:35:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+    return false if num < 2
+    i = 2
+      while i < num
+        if num % i == 0
+          return false
+        else
+          return true
+        end
+        i += 1
+      end
+   end
