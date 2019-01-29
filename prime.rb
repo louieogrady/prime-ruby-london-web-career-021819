7c2cef33b44de9d1e96d6f00eb890b4998490883
Louie O'Grady <l.ogrady@gmail.com>
2019-01-29 08:41:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
     return true if num == 2 || 3
     i = 4
       while i < num
-        if i % num == 0
+        if num - 1 % i == 0
           return false
         else
           return true
