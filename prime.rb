e92ec7d2b46619bdf531aa243d4771f1b45e2cc2
Louie O'Grady <l.ogrady@gmail.com>
2019-01-29 08:48:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,6 @@
 
-def prime?(number)
-  if number >= 2
-    (2..number - 1).all? do |x|
-      number % x != 0
-    end
-  else
-    return false
-  end
+
+def prime?(integer)
+  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
+  true
 end
