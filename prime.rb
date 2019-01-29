d360df0831bdf9f2e82d3f790c42e1f857568226
Louie O'Grady <l.ogrady@gmail.com>
2019-01-29 08:53:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,4 @@
 def prime?(integer)
-  (2..integer - 1).each { |x| return false if(integer % x) == 0}
+  (2..integer - 1).each { |x| return false if(integer % x) == 0 || integer <= 1}
   return true
 end
